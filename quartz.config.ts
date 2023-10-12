9e5d7ae69d7773d6cd8df9d099a3c49e989595f6
wansik.choi <wansik.choi@navercorp.com>
2023-10-12 15:05:06 +0900

    Quartz sync: Oct 12, 2023, 3:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Wansook World 🥚",
+    pageTitle: "🥚",
     appTitle: "Wansook World 🥚",
     description: "Wansook's Digital Lake.",
     enableSPA: true,
