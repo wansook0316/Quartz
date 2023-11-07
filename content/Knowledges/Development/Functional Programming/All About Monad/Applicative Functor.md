80ceb2666c39aa8e1d5dffa9d3e25adb53ae9429
wansik.choi <wansik.choi@navercorp.com>
2023-11-07 17:31:50 +0900

    Quartz sync: Nov 7, 2023, 5:31 PM

--- content/Knowledges/Development/Functional Programming/All About Monad/Applicative Functor.md
+++ content/Knowledges/Development/Functional Programming/All About Monad/Applicative Functor.md
@@ -9,6 +9,33 @@ tags:
 created: 2023-09-18
 ---
 
+Monad와 Functor 사이에는 Applicative Functor라는 중간 단계가 있다.
+
+# Applicative Functor
+
+ > 
+ > Monoidal Functor라는 것의 다른 이름
+
+# Monoidal Functor
+
+아무 **언어에서나** 정의될 수 있는 개념은 아니다. 다음의 조건이 선행되어야 한다.
+
+1. Empty Type $()\_{0}$ 을 제공해야 한다.
+   * `Void`
+   * `()`
+   * C에서 빈 구조체 등
+1. `T, U` 타입에 대한 Pair를 만들 수 있는 튜플 타입 $(T, U)\_{2}$를 제공해야 한다.
+1. 튜플 타입과 빈 타입은 다음의 함수를 제공해야 한다.
+   * `empty`: $() \rightarrow ()\_{0}$
+   * `unite`: $(T, U)*{2} \rightarrow (T, U)*{2}$
+   * `e1`: $(T, U)\_{2} \rightarrow T$
+   * `e2`: $(T, U)\_{2} \rightarrow U$
+
+즉, 다음을 만족해야 한다.
+
+ > 
+ > `unite(e1(u), e2(u)) = u`
+
 # Reference
 
 * [모나드의 모든 것](https://www.youtube.com/@antel588)
--- quartz/util/theme.ts
+++ quartz/util/theme.ts
@@ -4,6 +4,7 @@ export interface ColorScheme {
   gray: string
   darkgray: string
   dark: string
+  strong: string
   secondary: string
   tertiary: string
   highlight: string
@@ -40,6 +41,7 @@ ${stylesheet.join("\n\n")}
   --gray: ${theme.colors.lightMode.gray};
   --darkgray: ${theme.colors.lightMode.darkgray};
   --dark: ${theme.colors.lightMode.dark};
+  --strong: ${theme.colors.lightMode.strong};
   --secondary: ${theme.colors.lightMode.secondary};
   --tertiary: ${theme.colors.lightMode.tertiary};
   --highlight: ${theme.colors.lightMode.highlight};
@@ -55,6 +57,7 @@ ${stylesheet.join("\n\n")}
   --gray: ${theme.colors.darkMode.gray};
   --darkgray: ${theme.colors.darkMode.darkgray};
   --dark: ${theme.colors.darkMode.dark};
+  --strong: ${theme.colors.darkMode.strong};
   --secondary: ${theme.colors.darkMode.secondary};
   --tertiary: ${theme.colors.darkMode.tertiary};
   --highlight: ${theme.colors.darkMode.highlight};
